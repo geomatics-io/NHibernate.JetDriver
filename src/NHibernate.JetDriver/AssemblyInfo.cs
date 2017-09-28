8e34310e25d438597f52a23a04836bd08ae2e5ab
Christian Junk <christian.junk@amecfw.com>
2017-09-28 10:57:35 +0200

    Cleanup.

--- src/NHibernate.JetDriver/AssemblyInfo.cs
+++ src/NHibernate.JetDriver/AssemblyInfo.cs
@@ -4,14 +4,14 @@ using System.Security;
 
 [assembly: CLSCompliantAttribute(true)]
 [assembly: AssemblyTitleAttribute("NHibernate.JetDriver")]
-[assembly: AssemblyDescriptionAttribute("The Jet driver and dialect for NHibernate 4.1.0.4000.")]
+[assembly: AssemblyDescriptionAttribute("The Jet driver and dialect for NHibernate 4.0.0.4000.")]
 [assembly: AssemblyCompanyAttribute("NHibernate.org")]
 [assembly: AssemblyProductAttribute("NHibernate.JetDriver")]
 [assembly: AssemblyCopyrightAttribute("Licensed under LGPL.")]
 
-[assembly: AssemblyVersionAttribute("4.1.0.4000")]
-[assembly: AssemblyInformationalVersionAttribute("4.1.0.4000")]
-[assembly: AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: AssemblyVersionAttribute("4.0.0.4000")]
+[assembly: AssemblyInformationalVersionAttribute("4.0.0.4000")]
+[assembly: AssemblyFileVersionAttribute("4.0.0.4000")]
 
 [assembly: AssemblyDelaySignAttribute(false)]
 [assembly: AllowPartiallyTrustedCallers()]
