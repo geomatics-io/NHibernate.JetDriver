31e4937cc196ef0ae5116e809eb875a53a6042fb
Christian Junk <christian.junk@geomatics.io>
2020-01-22 20:48:04 +0100

    Fixing AppVeyor.

---
+++ src/NHibernate.JetDriver/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NHibernate.JetDriver")]
+[assembly: AssemblyDescription("The Jet driver and dialect for NHibernate 5.x.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("NHibernate.org")]
+[assembly: AssemblyProduct("NHibernate.JetDriver")]
+[assembly: AssemblyCopyright("Licensed under LGPL.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f7265fe1-7064-4f9d-86cb-86eebc844ba1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("5.0.0.0")]
+[assembly: AssemblyFileVersion("5.0.0.0")]
